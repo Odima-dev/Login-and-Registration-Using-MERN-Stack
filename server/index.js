96cd5fe21e1a32b22f1a6a6bde0fa549581ae95b
Odima-dev <vincentodima22@gmail.com>
2024-10-29 06:54:41 +0300

    updating signup page

--- server/index.js
+++ server/index.js
@@ -8,4 +8,10 @@ app.use(cors())
 
 mongoose.connect("mongodb://localhost:127.0.0.1:27017/employee");
 
-app.listen(3001, )
+app.post('/register', (req, res) => {
+    
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
