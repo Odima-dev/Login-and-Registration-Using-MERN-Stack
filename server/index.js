f3df616e80e2f1f1c47e07271b11493225b8393e
Odima-dev <vincentodima22@gmail.com>
2024-10-30 06:07:27 +0300

    update signup page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const cors = require("cors")
-const EmployeeModel = require('./models/Employee')
+const EmployeeModel = require('./models/Employee') 
+
 
 const app = express()
 app.use(express.json())
@@ -12,6 +13,7 @@ mongoose.connect("mongodb://localhost:127.0.0.1:27017/employee");
 app.post('/register', (req, res) => {
     EmployeeModel.create(req.body)
     .then(employees => res.json(employees))
+
     .catch(err => res.json(err))
 })
 
